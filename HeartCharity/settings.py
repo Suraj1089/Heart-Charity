32a18bad18c20a732897ce1f38d36b9014d3a7db
Suraj Pisal <surajpisal113@gmail.com>
2023-05-24 08:55:22 +0530

    Update settings.py

--- HeartCharity/settings.py
+++ HeartCharity/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'HeartCharity.wsgi.application'
 
 DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.sqlite3",
+            "ENGINE": "django.db.backends.postgresql",
             "NAME": os.getenv('DATABASE_URL')
         }
     }
